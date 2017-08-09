74943f0e5e47dc5e6b11e58e4c6386f199ece53e
Chris Dostert <chrisdostert@users.noreply.github.com>
2017-08-09 15:48:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ function createSasToken() {
     const fiveYears = (60 * 60 * 24 * 7) * 260;
     const ttl = Math.round(now.getTime() / 1000) + fiveYears;
 
-    const sbUrl = process.env.endpoint;
-    const httpsUrl = `https${sbUrl.substring(2)}events/messages`;
-    const encodedHttpsUrl = encodeURIComponent(httpsUrl);
+    const url = `${process.env.endpoint}events/messages`;
+    const encodedUrl = encodeURIComponent(url);
 
     // roundtrip to/from JSON to encode to UTF8 rather than depending on 3rd party module
-    const signatureUTF8 = JSON.parse(JSON.stringify(`${encodedHttpsUrl}\n${ttl}`));
+    const signatureUTF8 = JSON.parse(JSON.stringify(`${encodedUrl}\n${ttl}`));
 
     const hash = crypto.createHmac('sha256', process.env.sharedAccessKey).update(signatureUTF8).digest('base64');
-    return `SharedAccessSignature sr=${encodedHttpsUrl}&sig=${encodeURIComponent(hash)}&se=${ttl}&skn=${process.env.sharedAccessKeyName}`;
+    return `SharedAccessSignature sr=${encodedUrl}&sig=${encodeURIComponent(hash)}&se=${ttl}&skn=${process.env.sharedAccessKeyName}`;
 }
 
 console.log(`sasToken=${createSasToken()}`);
